78932fd5674af181e267db3876f934cfe5c396a4
haveyouwantto <haveyouwantto@gmail.com>
2023-07-17 23:56:43 +0800

    add url path detect

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import '../resources/waterfall.css';
 const app = new JMBoxApp('http://192.168.2.33:60752/');
 
 app.info();
+const path = location.hash.slice(1);
+app.setPath(path);
 app.list(true);
 
 window.app = app;
