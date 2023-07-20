f966f218c2f17e45033c3c7309e4844357652509
haveyouwantto <haveyouwantto@gmail.com>
2023-07-20 14:03:26 +0800

    add configure url

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import { JMBoxApp } from "./main/jmbox";
 import '../resources/style.css';
 import '../resources/waterfall.css';
 
-const app = new JMBoxApp();
+const url = localStorage.getItem('serverUrl');
+const app = url ? new JMBoxApp(url) : new JMBoxApp();
 
 app.info();
 const path = location.hash.slice(2);
